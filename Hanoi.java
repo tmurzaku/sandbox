3791f1cf3fef4c45638dee83b06ccc6203e17912
tmurzaku <159207608+tmurzaku@users.noreply.github.com>
2024-09-24 21:59:00 +0000

    Added the code for the hanoi problem

--- Hanoi.java
+++ Hanoi.java
@@ -2,6 +2,15 @@ public class Hanoi
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello");
+        solve(3, "A", "B", "C");
+    }
+    
+    public static void solve(int n, String src, String aux, String dst){
+        if (n == 0){
+            return;
+        }
+        solve(n-1, src, dst, aux);
+        System.out.println("Move " + src + "-> " + dst);
+        solve(n-1, aux, src, dst);
     }
 }
